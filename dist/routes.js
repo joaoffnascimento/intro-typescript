0f7f8096b68ec43eed7fdd42208ea65a983039c7
thecurrentuser <thecurrentuser@live.com>
2020-07-24 20:43:31 -0300

    buid

--- dist/routes.js
+++ dist/routes.js
@@ -1,9 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
-var CreateUser_1 = require("./services/CreateUser");
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helloWorld(req, res) {
-    return res.json(CreateUser_1.createUser({
+    return res.json(CreateUser_1.default({
         name: 'João Felipe',
         email: 'thecurrentuser@live.com',
         password: '123456',
--- dist/services/CreateUser.js
+++ dist/services/CreateUser.js
@@ -1,6 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUser = void 0;
+exports.returnAnotherCallBackExample = exports.overKillConsoleLog = void 0;
+/** Enums - Nomear conjunto de valores */
+var Techs;
+(function (Techs) {
+    Techs[Techs["React"] = 0] = "React";
+    Techs[Techs["Angular"] = 1] = "Angular";
+    Techs[Techs["Vue"] = 2] = "Vue";
+})(Techs || (Techs = {}));
 function createUser(_a) {
     var name = _a.name, email = _a.email, password = _a.password, techs = _a.techs;
     return {
@@ -10,4 +17,32 @@ function createUser(_a) {
         techs: techs
     };
 }
-exports.createUser = createUser;
+exports.default = createUser;
+function overKillConsoleLog(arg1, arg2) {
+    console.log(arg1, arg2);
+}
+exports.overKillConsoleLog = overKillConsoleLog;
+function returnAnotherCallBackExample(callback) {
+    return callback(9);
+}
+exports.returnAnotherCallBackExample = returnAnotherCallBackExample;
+var Balance = /** @class */ (function () {
+    function Balance() {
+        this.balance = 0;
+    }
+    Balance.prototype.increment = function (income) {
+        this.balance += income;
+    };
+    Balance.prototype.decrement = function (outcome) {
+        this.balance -= outcome;
+    };
+    return Balance;
+}());
+var picture = {
+    title: 'Profile',
+    description: "Picture taken for my driver's license",
+};
+var figure = {
+    title: 'Profile',
+    description: "Picture taken for my driver's license",
+};
